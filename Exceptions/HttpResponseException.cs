44608dbe186a01af8be05a8d1721f1debbb9c225
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-08 16:23:15 +0700

    add exception package

---
+++ Exceptions/HttpResponseException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+
+namespace BooruDex.Exceptions
+{
+	/// <summary>
+	/// Error that occur whenever <see cref="HttpResponseMessage"/> is not OK.
+	/// </summary>
+	public class HttpResponseException : Exception
+	{
+		/// <summary>
+		/// Create <see cref="HttpResponseException"/>.
+		/// </summary>
+		/// <param name="msg">Error message.</param>
+		public HttpResponseException(string msg) : base(msg)
+		{
+
+		}
+
+		/// <summary>
+		/// Create <see cref="HttpResponseException"/>.
+		/// </summary>
+		/// <param name="msg">Error message.</param>
+		/// <param name="e">Exception.</param>
+		public HttpResponseException(string msg, Exception e) : base(msg, e.InnerException)
+		{
+
+		}
+	}
+}
---
+++ Exceptions/SearchNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BooruDex.Exceptions
+{
+	/// <summary>
+	/// Error that occurs if no search results are found.
+	/// </summary>
+	public class SearchNotFoundException : Exception
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="msg"></param>
+		public SearchNotFoundException(string msg) : base(msg)
+		{
+
+		}
+	}
+}
