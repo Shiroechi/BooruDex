79f710c20b2c17a2d3a2223b0388abe477f7f1eb
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-10 09:32:03 +0700

    Create Danbooru.cs
    danbooru client

---
+++ Booru/Client/Danbooru.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace BooruDex.Booru.Client
+{
+	public class Danbooru : Template.Danbooru
+	{
+		/// <summary>
+		/// Create <see cref="Danbooru"/> client object.
+		/// </summary>
+		/// <param name="httpClient">Http client for sending request and recieving response.</param>
+		public Danbooru(HttpClient httpClient = null) : base("https://danbooru.donmai.us/", httpClient)
+		{
+
+		}
+	}
+}
