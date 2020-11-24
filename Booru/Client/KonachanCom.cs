869f821d73dbd2c3059022bc79314bc91768719c
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-24 18:49:23 +0700

    Create KonachanCom.cs

---
+++ Booru/Client/KonachanCom.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+
+using BooruDex.Booru.Template;
+
+namespace BooruDex.Booru.Client
+{
+	public class KonachanCom : Moebooru
+	{
+		#region Constructor & Destructor
+
+		/// <summary>
+		/// Create <see cref="KonachanCom"/> client object.
+		/// </summary>
+		/// <param name="httpClient">Http client for sending request and recieving response.</param>
+		public KonachanCom(HttpClient httpClient = null) : base("http://konachan.com/", httpClient)
+		{
+			this._PasswordSalt = "So-I-Heard-You-Like-Mupkids-?--{}--";
+		}
+
+		#endregion Constructor & Destructor
+	}
+}
