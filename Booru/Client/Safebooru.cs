4f942f68b4f6d2e21d1664d0b664e62897a8fb78
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-25 15:31:09 +0700

    Create Safebooru.cs

---
+++ Booru/Client/Safebooru.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+
+using BooruDex.Booru.Template;
+
+namespace BooruDex.Booru.Client
+{
+	public class Safebooru : Gelbooru02
+	{
+		#region Constructor & Destructor
+
+		/// <summary>
+		/// Create <see cref="Safebooru"/> client object.
+		/// </summary>
+		/// <param name="httpClient">Http client for sending request and recieving response.</param>
+		public Safebooru(HttpClient httpClient = null) : base("https://safebooru.org/", httpClient)
+		{
+			this.IsSafe = true;
+		}
+
+		#endregion Constructor & Destructor
+	}
+}
