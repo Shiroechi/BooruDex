511199165c24099911277985b340178708864626
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-25 08:53:05 +0700

    Danbooru: override read json

--- Booru/Template/Danbooru.cs
+++ Booru/Template/Danbooru.cs
@@ -1,7 +1,11 @@
 ﻿using System.Net.Http;
 
+using BooruDex.Models;
+
 using Litdex.Security.RNG;
 using Litdex.Security.RNG.PRNG;
+
+using Newtonsoft.Json.Linq;
 
 namespace BooruDex.Booru.Template
 {
@@ -56,6 +60,74 @@ namespace BooruDex.Booru.Template
 				return $"{ this._BaseUrl.AbsoluteUri }{ query }.json?";
 			}
 			return $"{ this._BaseUrl.AbsoluteUri }{ query }.xml?";
+		}
+
+		/// <inheritdoc/>
+		protected override Artist ReadArtist(JToken json)
+		{
+			return new Artist(
+				json["id"].Value<uint>(),
+				json["name"].Value<string>(),
+				null); // no artist urls.
+		}
+
+		/// <inheritdoc/>
+		protected override Pool ReadPool(JToken json)
+		{
+			return new Pool(
+				json["id"].Value<uint>(),
+				json["name"].Value<string>(),
+				json["post_count"].Value<uint>(),
+				json["description"].Value<string>());
+		}
+
+		/// <inheritdoc/>
+		protected override Post ReadPost(JToken json)
+		{
+			return new Post(
+				json["id"].Value<uint>(),
+				this._BaseUrl + "posts/",
+				json["file_url"].Value<string>(),
+				json["preview_file_url"].Value<string>(),
+				this.ConvertRating(json["rating"].Value<string>()),
+				json["tag_string"].Value<string>(),
+				json["file_size"].Value<uint>(),
+				json["image_height"].Value<int>(),
+				json["image_width"].Value<int>(),
+				0,
+				0,
+				json["source"].Value<string>()
+				);
+		}
+
+		/// <inheritdoc/>
+		protected override Tag ReadTag(JToken json)
+		{
+			return new Tag(
+				json["id"].Value<uint>(),
+				json["name"].Value<string>(),
+				(TagType)json["category"].Value<int>(),
+				json["post_count"].Value<uint>()
+				);
+		}
+
+		/// <inheritdoc/>
+		protected override TagRelated ReadTagRelated(JToken json)
+		{
+			var item = (JArray)json;
+			return new TagRelated(
+				item[0].Value<string>(),
+				item[1].Value<uint>());
+		}
+
+		/// <inheritdoc/>
+		protected override Wiki ReadWiki(JToken json)
+		{
+			return new Wiki(
+				json["id"].Value<uint>(),
+				json["title"].Value<string>(),
+				json["body"].Value<string>()
+				);
 		}
 
 		#endregion Protected Overrride Method
