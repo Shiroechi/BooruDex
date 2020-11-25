503e9c05b9a8df5036f15e7236eaa8e3df4483e8
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-25 13:58:51 +0700

    Create Gelbooru02.cs

---
+++ Booru/Template/Gelbooru02.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+
+using Litdex.Security.RNG;
+using Litdex.Security.RNG.PRNG;
+
+namespace BooruDex.Booru.Template
+{
+	/// <summary>
+	/// Gelbooru beta version 0.2.0.
+	/// </summary>
+	public abstract class Gelbooru02 : Booru
+	{
+		#region Constructor & Destructor
+
+		/// <summary>
+		/// <see cref="Gelbooru02"/> template for booru client.
+		/// </summary>
+		/// <param name="domain">URL of booru based sites.</param>
+		/// <param name="httpClient">Client for sending and receive http response.</param>
+		public Gelbooru02(string domain, HttpClient httpClient = null) : this(domain, httpClient, new SplitMix64())
+		{
+
+		}
+
+		/// <summary>
+		/// <see cref="Gelbooru02"/> template for booru client.
+		/// </summary>
+		/// <param name="domain">URL of booru based sites.</param>
+		/// <param name="httpClient">Client for sending and receive http response.</param>
+		/// <param name="rng">Random generator for random post.</param>
+		public Gelbooru02(string domain, HttpClient httpClient, IRNG rng) : base(domain, httpClient, rng)
+		{
+			this._PostLimit = 100;
+			this._TagsLimit = 0; // no tag limit
+			this._PageLimit = 10;
+			this.IsSafe = false;
+			this._ApiVersion = "";
+			this._PasswordSalt = "";
+		}
+
+		/// <summary>
+		/// Release all resource that this object hold.
+		/// </summary>
+		~Gelbooru02()
+		{
+
+		}
+
+		#endregion Constructor & Destructor
+
+	}
+}
