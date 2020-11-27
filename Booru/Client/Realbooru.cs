06ca00be087aa7657347f219341fad741deffc21
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-27 19:46:52 +0700

    Realbooru: override ReadPost

--- Booru/Client/Realbooru.cs
+++ Booru/Client/Realbooru.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net.Http;
+using System.Text.Json;
 
 using BooruDex2.Booru.Template;
+using BooruDex2.Models;
 
 namespace BooruDex2.Booru.Client
 {
@@ -18,5 +20,30 @@ namespace BooruDex2.Booru.Client
 		}
 
 		#endregion Constructor & Destructor
+
+		#region Protected Override Method
+
+		protected override Post ReadPost(JsonElement json)
+		{
+			var imageName = json.GetProperty("image").GetString();
+
+			var directory = json.GetProperty("directory").GetInt32();
+
+			return new Post(
+				json.GetProperty("id").GetUInt32(),
+				this._BaseUrl + "index.php?page=post&s=view&id=",
+				this._BaseUrl + "/images/" + directory + "/" + imageName,
+				this._BaseUrl + "/thumbnails/" + directory + "/thumbnails_" + imageName.Substring(0, imageName.IndexOf(".")) + ".jpg",
+				this.ConvertRating(json.GetProperty("rating").GetString()),
+				json.GetProperty("tags").GetString(),
+				0,
+				json.GetProperty("height").GetInt32(),
+				json.GetProperty("width").GetInt32(),
+				0,
+				0,
+				string.Empty);
+		}
+
+		#endregion Protected Override Method
 	}
 }
