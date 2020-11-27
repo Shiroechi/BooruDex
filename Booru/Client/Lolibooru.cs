832b8df75c37aabbddd66bc98b823670e8d961c1
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-27 17:22:55 +0700

    Lolibooru: override all Read Json
    all json response from lolibooru, all property value always string

--- Booru/Client/Lolibooru.cs
+++ Booru/Client/Lolibooru.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Text.Json;
 
 using BooruDex2.Booru.Template;
@@ -27,9 +28,29 @@ namespace BooruDex2.Booru.Client
 		#region Protected Override Method
 
 		/// <inheritdoc/>
+		protected override Artist ReadArtist(JsonElement json)
+		{
+			var array = json.GetProperty("urls");
+
+			var urls = new List<string>();
+
+			if (array.GetArrayLength() != 0)
+			{
+				foreach (var item in array.EnumerateArray())
+				{
+					urls.Add(item.GetString());
+				}
+			}
+
+			return new Artist(
+				uint.Parse(json.GetProperty("id").GetString()),
+				json.GetProperty("name").GetString(),
+				urls);
+		}
+
+		/// <inheritdoc/>
 		protected override Tag ReadTag(JsonElement json)
 		{
-			var item = json;
 			return new Tag(
 				json.GetProperty("id").GetUInt32(),
 				json.GetProperty("name").GetString(),
