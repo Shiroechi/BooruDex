b1f5ec83a5ec901a3d11b71782aeb9663eb6110e
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-08 10:09:12 +0700

    Realbooru: update page limit

--- Booru/Client/Realbooru.cs
+++ Booru/Client/Realbooru.cs
@@ -16,7 +16,7 @@ namespace BooruDex.Booru.Client
 		/// <param name="httpClient">Http client for sending request and recieving response.</param>
 		public Realbooru(HttpClient httpClient = null) : base("https://realbooru.com/", httpClient)
 		{
-
+			this._PageLimit = 200000;
 		}
 
 		#endregion Constructor & Destructor
