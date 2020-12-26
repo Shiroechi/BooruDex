a261c30cb2fe945b3e4e1b1f9823f16ed45a97df
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-26 16:18:25 +0700

    Update Artist.cs

--- Models/Artist.cs
+++ Models/Artist.cs
@@ -4,16 +4,22 @@ using System.Collections.ObjectModel;
 namespace BooruDex.Models
 {
 	/// <summary>
-	/// Represents a Artist object.
+	///		Represents a Artist object.
 	/// </summary>
 	readonly public struct Artist
 	{
 		/// <summary>
-		/// 
+		///		Initialize <see cref="Artist"/> instance.
 		/// </summary>
-		/// <param name="id"><see cref="Artist"/> id.</param>
-		/// <param name="name"><see cref="Artist"/> name.</param>
-		/// <param name="urls">List of <see cref="Artist"/> urls.</param>
+		/// <param name="id">
+		///		The <see cref="Artist"/> id.
+		///	</param>
+		/// <param name="name">
+		///		The <see cref="Artist"/> name.
+		///	</param>
+		/// <param name="urls">
+		///		List of <see cref="Artist"/> urls.
+		///	</param>
 		public Artist(uint id, string name, IList<string> urls)
 		{
 			this.ID = id;
@@ -22,17 +28,17 @@ namespace BooruDex.Models
 		}
 
 		/// <summary>
-		/// Gets the ID of the artist.
+		///		Gets the ID of the artist.
 		/// </summary>
 		public uint ID { get; }
 
 		/// <summary>
-		/// Gets the name of the artist.
+		///		Gets the name of the artist.
 		/// </summary>
 		public string Name { get; }
 
 		/// <summary>
-		/// List of <see cref="Artist"/> official url.
+		///		List of <see cref="Artist"/> official url.
 		/// </summary>
 		public ReadOnlyCollection<string> Urls { get; }
 
