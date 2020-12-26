1494e831c7d425d2783012648fced4c5a1502d9d
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-26 16:21:03 +0700

    Update Pool.cs

--- Models/Pool.cs
+++ Models/Pool.cs
@@ -1,10 +1,25 @@
 ﻿namespace BooruDex.Models
 {
 	/// <summary>
-	/// Represents a Artist object.
+	///		Represents a Artist object.
 	/// </summary>
 	readonly public struct Pool
 	{
+		/// <summary>
+		///		Initialize <see cref="Pool"/> instance.
+		/// </summary>
+		/// <param name="id">
+		///		The <see cref="Pool"/> id.
+		/// </param>
+		/// <param name="name">
+		///		The <see cref="Pool"/> name or title.
+		/// </param>
+		/// <param name="postCount">
+		///		Number of <see cref="Post"/> in the <see cref="Pool"/>.
+		/// </param>
+		/// <param name="description">
+		///		Description of the <see cref="Pool"/>.
+		/// </param>
 		public Pool(uint id, string name, uint postCount, string description)
 		{
 			this.ID = id;
@@ -14,22 +29,22 @@
 		}
 
 		/// <summary>
-		/// Gets the ID of the pool.
+		///		Gets the ID of the <see cref="Pool"/>.
 		/// </summary>
 		public uint ID { get; }
 
 		/// <summary>
-		/// Gets the name of the pool.
+		///		Gets the name of the <see cref="Pool"/>.
 		/// </summary>
 		public string Name { get; }
 
 		/// <summary>
-		/// Gets the number of post in the pool.
+		///		Gets the number of <see cref="Post"/> in the <see cref="Pool"/>.
 		/// </summary>
 		public uint PostCount { get; }
 
 		/// <summary>
-		/// Gets the description of the pool.
+		///		Gets the description of the <see cref="Pool"/>.
 		/// </summary>
 		public string Description { get; }
 
