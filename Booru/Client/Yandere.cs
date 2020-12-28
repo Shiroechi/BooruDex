948e75e791c67c8430652055bee61202a8ca886a
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-28 10:44:08 +0700

    Update Yandere.cs

--- Booru/Client/Yandere.cs
+++ Booru/Client/Yandere.cs
@@ -9,9 +9,11 @@ namespace BooruDex.Booru.Client
 		#region Constructor & Destructor
 
 		/// <summary>
-		/// Create <see cref="Yandere"/> client object.
+		///		Create <see cref="Yandere"/> client object.
 		/// </summary>
-		/// <param name="httpClient">Http client for sending request and recieving response.</param>
+		/// <param name="httpClient">
+		///		Http client for sending request and recieving response.
+		///	</param>
 		public Yandere(HttpClient httpClient = null) : base("https://yande.re/", httpClient)
 		{
 			this._PasswordSalt = "choujin-steiner--{}--";
