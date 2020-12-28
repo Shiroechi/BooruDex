8a5f8b5e16ce5449eacea81d1505608369f29285
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-28 10:44:00 +0700

    Update SafebooruDonmai.cs

--- Booru/Client/SafebooruDonmai.cs
+++ Booru/Client/SafebooruDonmai.cs
@@ -7,9 +7,11 @@ namespace BooruDex.Booru.Client
 		#region Constructor & Destructor
 
 		/// <summary>
-		/// Create <see cref="SafebooruDonmai"/> client object.
+		///		Create <see cref="SafebooruDonmai"/> client object.
 		/// </summary>
-		/// <param name="httpClient">Http client for sending request and recieving response.</param>
+		/// <param name="httpClient">
+		///		Http client for sending request and recieving response.
+		///	</param>
 		public SafebooruDonmai(HttpClient httpClient = null) : base("https://safebooru.donmai.us/", httpClient)
 		{
 			this.IsSafe = true;
