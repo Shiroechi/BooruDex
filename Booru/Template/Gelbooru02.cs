d789fd9d392be1f715b7998a327c792ead9c19a6
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-31 14:47:58 +0700

    Update Gelbooru02.cs

--- Booru/Template/Gelbooru02.cs
+++ Booru/Template/Gelbooru02.cs
@@ -60,19 +60,21 @@ namespace BooruDex.Booru.Template
 
 			var directory = json.GetProperty("directory").GetString();
 
-			return new Post(
-				json.GetProperty("id").GetUInt32(),
-				this._BaseUrl + "index.php?page=post&s=view&id=",
-				this._BaseUrl + "images/" + directory + "/" + imageName,
-				this._BaseUrl + "thumbnails/" + directory + "/thumbnail_" + imageName.Substring(0, imageName.IndexOf(".")) + ".jpg",
-				this.ConvertRating(json.GetProperty("rating").GetString()),
-				json.GetProperty("tags").GetString(),
-				0,
-				json.GetProperty("height").GetInt32(),
-				json.GetProperty("width").GetInt32(),
-				0,
-				0,
-				string.Empty);
+			return new Post
+			{
+				ID = json.GetProperty("id").GetUInt32(),
+				PostUrl = this._BaseUrl + "index.php?page=post&s=view&id=",
+				FileUrl = this._BaseUrl + "images/" + directory + "/" + imageName,
+				PreviewUrl = this._BaseUrl + "thumbnails/" + directory + "/thumbnail_" + imageName.Substring(0, imageName.IndexOf(".")) + ".jpg",
+				Rating = this.ConvertRating(json.GetProperty("rating").GetString()),
+				Tags = json.GetProperty("tags").GetString(),
+				Size = 0,
+				Height = json.GetProperty("height").GetInt32(),
+				Width = json.GetProperty("width").GetInt32(),
+				PreviewHeight = 0,
+				PreviewWidth = 0,
+				Source = string.Empty
+			};
 		}
 
 		#endregion Protected Overrride Method
